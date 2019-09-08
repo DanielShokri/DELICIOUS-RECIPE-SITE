a1073f9bfe7dda336dad279bb74927e03ef354f2
Daniel Shokri <danielshokri@gmail.com>
2019-09-08 17:30:12 +0300

    Updates

---
+++ precache-manifest.ffa96355dac58111237eaf20be8c2181.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de298ff6a3c7788942181ccd886ca6d8",
+    "url": "/DELICIOUS-RECIPE-SITE/index.html"
+  },
+  {
+    "revision": "2526731b70fd19b7cffe",
+    "url": "/DELICIOUS-RECIPE-SITE/static/css/2.c0851dc3.chunk.css"
+  },
+  {
+    "revision": "29c1b0fab574866ae1b4",
+    "url": "/DELICIOUS-RECIPE-SITE/static/css/main.3e21015d.chunk.css"
+  },
+  {
+    "revision": "2526731b70fd19b7cffe",
+    "url": "/DELICIOUS-RECIPE-SITE/static/js/2.bb25ddd2.chunk.js"
+  },
+  {
+    "revision": "29c1b0fab574866ae1b4",
+    "url": "/DELICIOUS-RECIPE-SITE/static/js/main.2f42837c.chunk.js"
+  },
+  {
+    "revision": "dc66cf77b5da5fcbc6f7",
+    "url": "/DELICIOUS-RECIPE-SITE/static/js/runtime~main.0dd3c454.js"
+  }
+]);
